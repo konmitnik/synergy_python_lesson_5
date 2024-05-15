fd2c887b65bb5f18d4163409e157694fa751a674
konmitnik <konmitnik@gmail.com>
2024-05-15 21:06:42 +0300

    update task2

--- task2.py
+++ task2.py
@@ -9,4 +9,6 @@ for char in word:
         count_consonant += 1
 
 print(f"Число согласных: {count_consonant}\nЧисло гласных: {count_vowels if count_vowels > 0 else False}")
+if count_vowels > 0:
+    print(f"Количество гласных: a - {word.count('a')}, e - {word.count('e')}, i - {word.count('i')}, o - {word.count('o')}, u - {word.count('u')}")
 
